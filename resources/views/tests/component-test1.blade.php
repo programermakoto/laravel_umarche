450dfa2d492b3cbd7c60e34968d2e868587d0908
makoto <makoto17720620@gmail.com>
2023-11-22 12:25:25 +0900

    属性設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+    <x-slot name="header">header1</x-slot>
 コンポーネントテスト１
+<x-tests.card title="タイトル1" content="本文1" />
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
-コンポーネントテスト2
+    <x-slot name="header">header1</x-slot>
+    コンポーネントテスト2
 </x-tests.app>
