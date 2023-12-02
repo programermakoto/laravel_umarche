83f3e9c9f7e10083e5193fc13ed2c6a63352e4df
makoto <makoto17720620@gmail.com>
2023-12-02 17:47:08 +0900

    multiLogin

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,5 +1,7 @@
 <x-tests.app>
     <x-slot name="header">header1</x-slot>
 コンポーネントテスト１
-<x-tests.card title="タイトル1" content="本文1" />
+
+<x-tests.card title="タイトル1" content="本文1"></x-tests.card>
+
 </x-tests.app>
