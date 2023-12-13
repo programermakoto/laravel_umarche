50451f0abb7e97d81cb3ae928ede80653bd8f369
makoto <makoto17720620@gmail.com>
2023-12-13 11:21:57 +0900

    カーボン

--- database/migrations/2023_11_22_171426_create_admins_table.php
+++ database/migrations/2023_11_22_171426_create_admins_table.php
@@ -20,6 +20,7 @@ class CreateAdminsTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->timestamps();
         });
     }
 
