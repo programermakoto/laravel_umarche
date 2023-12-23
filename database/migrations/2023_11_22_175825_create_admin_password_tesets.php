ee234d5356ddf5a5f1c7bd8f1dcc40f40292e7ab
makoto <makoto17720620@gmail.com>
2023-12-23 11:36:11 +0900

    途中経過

--- database/migrations/2023_11_22_175825_create_admin_password_tesets.php
+++ database/migrations/2023_11_22_175825_create_admin_password_tesets.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminPasswordTesets extends Migration
+class CreateAdminPasswordTests extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAdminPasswordTesets extends Migration
      */
     public function up()
     {
-        Schema::create('admin_password_tesets', function (Blueprint $table) {
+        Schema::create('admin_password_tests', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
