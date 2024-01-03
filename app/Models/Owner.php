61b35e21b99a5c892fe70593cb4e3b7296140d1b
makoto <makoto17720620@gmail.com>
2024-01-03 21:51:28 +0900

    shop

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\shop;
 class Owner extends Authenticatable
 {
     use HasFactory,SoftDeletes;
@@ -38,4 +39,7 @@ class Owner extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function shop(){
+        return $this->hasOne(shop::class);
+    }
 }
