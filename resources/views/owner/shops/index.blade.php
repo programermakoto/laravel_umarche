27dbdfd629eba9db2646f56929c761ef3b35be65
makoto <makoto17720620@gmail.com>
2024-01-06 00:23:21 +0900

    error0106

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -8,7 +8,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    
                     @foreach ($shops as $shop)
                         <div class="w-1/2 p-2">
 
@@ -49,3 +48,21 @@
             </div>
         </div>
 </x-app-layout>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
