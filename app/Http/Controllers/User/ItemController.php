6d3e4d3777313c875b72d41ce8c0102ed7bbf80d
makoto <makoto17720620@gmail.com>
2024-02-09 16:23:43 +0900

    クエリ制作

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -18,31 +18,32 @@ class ItemController extends Controller
         // ->groupBy('product_id')->having('quantity','>',1);でproduct_id毎の合計在庫が１個以上あるかを指定
 
         $stocks = DB::table('t_stocks')
-            ->select('product_id',
-             DB::raw('sum(quantity) as quantity'))
+            ->select(
+                'product_id',
+                DB::raw('sum(quantity) as quantity')
+            )
             ->groupBy('product_id')
-            ->having('quantity', '>=',1);
+            ->having('quantity', '>', 1);
 
         $products = DB::table('products')
             ->joinSub($stocks, 'stock', function ($join) {
-                $join->on('products.id','=','stock.product_id');
+                $join->on('products.id', '=', 'stock.product_id');
             })
 
-            ->join('shops', 'products.shop_id','=','shops.id')
-            ->join('secondary_categories','products.secondary_category_id','=','secondary_categories.id')
-            ->join('images as image1','products.image1','=','image1.id')
-            ->join('images as image2','products.image2','=','image2.id')
-            ->join('images as image3','products image3','=','image3.id')
-            ->join('images as image4','products image4','=','image4.id')
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
             ->where('shops.is_selling', true)
             ->where('products.is_selling', true)
-            ->select('products.id as id'
-            , 'products.name as name'
-            , 'products.price'
-            ,'products.sort_order as sort_order'
-            ,'products.information'
-            ,'secondary_categories.name as category'
-            ,'image1.filename as filename')
+            ->select(
+                'products.id as id',
+                'products.name as name',
+                'products.price',
+                'products.sort_order as sort_order',
+                'products.information',
+                'secondary_categories.name as category',
+                'image1.filename as filename'
+            )
             ->get();
         // dd($stocks,$products);
 
