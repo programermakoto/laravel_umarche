0dafb5a1bfae35fa08e47bbc501ecd321276f655
programermakoto <makoto17720620@gmail.com>
2024-03-16 13:35:14 +0900

    localscope

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -18,43 +18,7 @@ class ItemController extends Controller
     }
     public function index()
     {
-        //table('t_stocks')でテーブル名を取得する
-
-        // DB::rawでSQLをそのままファイルに記述できる
-
-        // ->groupBy('product_id')->having('quantity','>',1);でproduct_id毎の合計在庫が１個以上あるかを指定
-
-        $stocks = DB::table('t_stocks')
-            ->select(
-                'product_id',
-                DB::raw('sum(quantity) as quantity')
-            )
-            ->groupBy('product_id')
-            ->having('quantity', '>', 1);
-
-        $products = DB::table('products')
-            ->joinSub($stocks, 'stock', function ($join) {
-                $join->on('products.id', '=', 'stock.product_id');
-            })
-
-            ->join('shops', 'products.shop_id', '=', 'shops.id')
-            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
-            ->join('images as image1', 'products.image1', '=', 'image1.id')
-            ->where('shops.is_selling', true)
-            ->where('products.is_selling', true)
-            ->select(
-                'products.id as id',
-                'products.name as name',
-                'products.price',
-                'products.sort_order as sort_order',
-                'products.information',
-                'secondary_categories.name as category',
-                'image1.filename as filename'
-            )
-            ->get();
-        // dd($stocks,$products);
-
-        // $products = Product::all();
+        $products = Product::availableItems()->get();
         return view('user.index', compact('products'));
     }
     public function show($id)
--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -8,10 +8,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-<<<<<<< HEAD
-=======
                     <x-flash-message status="session('status')" />
->>>>>>> origin/sec07_others
                     @foreach ($shops as $shop)
                         <div class="w-1/2 p-2">
 
