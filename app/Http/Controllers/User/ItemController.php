185a3c566f143d49bbfa16f1fa7983971a0f7703
programermakoto <makoto17720620@gmail.com>
2024-03-19 18:20:40 +0900

    TestMail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use App\Models\Stock;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -20,37 +22,37 @@ class ItemController extends Controller
 
             $id = $request->route()->parameter("item"); //itemのidを取得
 
-           // routes/web.phpのRoute::get("show/{item}",[ItemController::class,"show"])の事
+            // routes/web.phpのRoute::get("show/{item}",[ItemController::class,"show"])の事
 
-           if (!is_null($id)) { //itemのidが存在するなら↓
+            if (!is_null($id)) { //itemのidが存在するなら↓
 
-            $itemId = Product::availableItems()->where('products.id',$id)->exists(); //productのidが入ってきた値idと一致してるか。入ってきた値が存在するかどうか確認。
+                $itemId = Product::availableItems()->where('products.id', $id)->exists(); //productのidが入ってきた値idと一致してるか。入ってきた値が存在するかどうか確認。
 
-           //↓itemIdが存在していなかったら
+                //↓itemIdが存在していなかったら
 
-           if (!$itemId) {
+                if (!$itemId) {
 
-           abort(404); //404の画面表示
+                    abort(404); //404の画面表示
 
+                }
             }
 
-            }
-
-           return $next($request);
-
-            });
+            return $next($request);
+        });
     }
 
     public function index(Request $request)
     {
         // dd($request);
+        Mail::to('test@example.com') //受信者の指定
+        ->send(new TestMail()); //Mailableクラス
         $categories = PrimaryCategory::with("secondary")->get();
         $products = Product::availableItems()
-        ->searchKeyword($request->keyword)
-        ->selectCategory($request->category ?? "0")//選んでいない場合初期値０に！
-        ->sortOrder($request->sort)
-        ->paginate($request->pagination ?? '20');
-        return view('user.index', compact('products','categories'));
+            ->searchKeyword($request->keyword)
+            ->selectCategory($request->category ?? "0") //選んでいない場合初期値０に！
+            ->sortOrder($request->sort)
+            ->paginate($request->pagination ?? '20');
+        return view('user.index', compact('products', 'categories'));
     }
     public function show($id)
     {
@@ -58,15 +60,10 @@ class ItemController extends Controller
         $product = Product::findOrFail($id);
         $quantity = Stock::where("product_id", $product->id)->sum("quantity"); //一つの商品の在庫情報を取るために
 
-        if($quantity>9){
-            $quantity=9;
-        }//9より大きかったら９
+        if ($quantity > 9) {
+            $quantity = 9;
+        } //9より大きかったら９
 
-        return view('user.show', compact('product','quantity'));
+        return view('user.show', compact('product', 'quantity'));
     }
 }
-
-
-
-
-
